9904ce878ba0bea3deb4fa2716e901a50d450a33
Ydmundi <perezydmundi@gmail.com>
2025-01-02 20:36:27 +0800

    Update
    Email Content Update to filename

--- src/api/forms-list/services/forms-list.js
+++ src/api/forms-list/services/forms-list.js
@@ -146,7 +146,7 @@ module.exports = createCoreService('api::forms-list.forms-list', ({ strapi }) =>
         // FETCH SIGNATURE WITHOUT BACKGROUND
         // const imagePath = `public/uploads/${data.user.signature.hash}${data.user.signature.ext}`
         let signatureContent;
-        console.log(data);
+        // console.log(data);
         if(data.user.signature){
           // If the image exists, use the image configuration
           signatureContent = {
